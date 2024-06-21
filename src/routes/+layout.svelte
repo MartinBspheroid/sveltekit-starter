a7fbdc8baea8a21e1349061de651a6af071c1707
Martin Blasko <martin.blasko@lifeliqe.com>
2024-06-21 15:22:09 +0200

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+	import { setMode, ModeWatcher } from 'mode-watcher';
+	setMode('dark');
+</script>
+
+<ModeWatcher />
+<div class="flex flex-col items-center  min-h-screen py-2">
+<slot />
+
+</div>
