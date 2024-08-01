28d99b2b72a337d92941bd855d572d7da1223c22
Martin Blasko <mblasko42@gmail.com>
2024-08-01 17:22:10 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		open: true,
+		port: 2000,
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
